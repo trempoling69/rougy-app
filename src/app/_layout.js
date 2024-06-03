a67931d0bc2c0127297f748ea6567f5a6b2da578
trempoling69 <romain.mourieras@gmail.com>
2024-06-03 11:41:12 -0400

    update to latest version

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -2,16 +2,19 @@ import { Slot } from 'expo-router';
 import { SessionProvider } from '../context/ctx';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { CartProvider } from '../context/cartContext';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Root() {
   // Set up the auth context and render our layout inside of it.
   return (
-    <SessionProvider>
-      <CartProvider>
-        <BottomSheetModalProvider>
-          <Slot screenOptions={{ color: 'red' }} />
-        </BottomSheetModalProvider>
-      </CartProvider>
-    </SessionProvider>
+    <GestureHandlerRootView>
+      <SessionProvider>
+        <CartProvider>
+          <BottomSheetModalProvider>
+            <Slot screenOptions={{ color: 'red' }} />
+          </BottomSheetModalProvider>
+        </CartProvider>
+      </SessionProvider>
+    </GestureHandlerRootView>
   );
 }
